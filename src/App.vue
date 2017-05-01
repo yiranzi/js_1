0111507f98b4d84a2530e3bbdb50f884c35b4fba
yiranzi <570932296@qq.com>
2017-05-01 10:37:34 +0800

    增加js的练习脚本

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,15 @@
   <div id="app">
     <div>
       <span>1234</span>
-      <span>{{msg}}</span>
+      <!--需要一个对象 key是class 值是true false-->
+      <p :class="{'test': isTest}">{{msg}}</p>
+<!--直接传入一个对象也可以-->
+      <p :class="testObject">{{msg}}</p>
+      <p :class="testCalc">{{msg}}</p>
+      <p :class="testMethod(1)">{{msg}}</p>
+      <p :class="[testMethod(1),testCalc]">{{msg}}</p>
+
+      <span>{{myCalc}}</span>
       <first v-for="(course, key) in course" :courseProcess = course></first>
       <!--<first :courseProcess = course></first>-->
     </div>
@@ -15,6 +23,11 @@ export default {
   name: 'app',
   data () {
     return {
+        testObject: {
+            'test2': true,
+        },
+        isTest: true,
+        number: 10,
       msg: 'Welcome to Your Vue.js App',
       course: [
           {
@@ -74,11 +87,34 @@ export default {
   },
   components: {
       first,
-  }
+  },
+    computed: {
+      myCalc () {
+          return this.number + 1
+      },
+        testCalc: function(){
+          return{
+              'test': true
+          }
+        }
+    },
+    methods: {
+      testMethod: function(index){
+          return{
+              'test2': index === 1
+          }
+      }
+    }
 }
 </script>
 
 <style>
+  .test{
+    font-size: 30px;
+  }
+  .test2{
+    color: green;
+  }
 </style>
 
 
---
+++ src/normal/first.js
@@ -0,0 +1,13 @@
+/**
+ * Created by wuchuck on 4/26/17.
+ */
+
+for (var i = 0; i < 10; i++) {
+    for (var j = 0; j<10; j++)
+    var div = document.getElementById('first');
+
+    var ele = document.createElement('span');
+    ele.innerHTML ='';
+    div.appendChild(ele);
+    console.log(i)
+}
