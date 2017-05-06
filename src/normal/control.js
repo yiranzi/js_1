96a49ebb93fe8a9e283c0e70907786d9911ae790
yiranzi <570932296@qq.com>
2017-05-06 13:00:05 +0800

    修改移动方向的bug

--- src/normal/control.js
+++ src/normal/control.js
@@ -7,20 +7,30 @@ document.onkeydown = function (ev) {
 
     switch (num) {
         case 87:
-            dirX = 0
-            dirY = -1
+            if(dirY != 1){
+                dirX = 0
+                dirY = -1
+            }
             break;
         case 83:
-            dirX = 0
-            dirY = 1
+            if(dirY != -1){
+                dirX = 0
+                dirY = 1
+            }
             break;
         case 65:
-            dirX = -1
-            dirY = 0
+            if(dirX != 1){
+                dirX = -1
+                dirY = 0
+            }
+
             break;
         case 68:
-            dirX = 1
-            dirY = 0
+            if(dirX != -1){
+                dirX = 1
+                dirY = 0
+            }
+
             break;
     }
 }
